49f095533901f3d5396eacba63463249759079cd
AsherThomasBabu <ashertb2000@gmail.com>
2021-04-23 19:21:44 +0530

    add problem statement

--- Sorted-Squared-Array/naive.py
+++ Sorted-Squared-Array/naive.py
@@ -1,4 +1,4 @@
-
+# Write a function that takes in a non-empty array of Integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
 
 def sortedSquaredArray(array):
     # Write your code here.
