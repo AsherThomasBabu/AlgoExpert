d3e77f5dd3b41e4e82df350fcdb5695ec0b6cd38
AsherThomasBabu <ashertb2000@gmail.com>
2021-04-25 10:32:23 +0530

    added nonConstructibleChange - optimised

---
+++ Non-Constructible Change/optimised.py
@@ -0,0 +1,16 @@
+# Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) that you cannot create. The given coins can have any positive integer value and aren't necessarily unique (.e., you can have multiple coins of the same value). 
+
+# For example, if you're given coins = [1, 2, 5], the minimum amount of change that you can't create is 4. If you're given no coins, the minimum amount of change that you can't create is 1.
+
+
+def nonConstructibleChange(array):
+    maxChange = 0
+    array.sort()
+    for i in array:
+        if i <= maxChange+1:
+            maxChange += i
+        elif i > maxChange+1:
+            break
+
+    return (maxChange+1)
+
