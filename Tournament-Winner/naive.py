6ed050524725aab9866498af5bb9e6e9e4f5241e
AsherThomasBabu <ashertb2000@gmail.com>
2021-04-25 13:00:48 +0530

    added tournamentWinner - naive

---
+++ Tournament-Winner/naive.py
@@ -0,0 +1,20 @@
+# There's an algorithms tournament taking place in which teams of programmers compete against each other to solve algorithmic problems as fast as possible. Teams compete in a round robin, where each team faces off against all other teams. Only two teams compete against each other at a time, and for each competition, one team is designated the home team, while the other team is the away team. In each competition there's always one winner and one loser; there are no ties. A team receives 3 points if it wins and 0 points if it loses. The winner of the tournament is he team that receives the most amount of points. Given an array of pairs representing the teams that have competed against each other and an array containing the results of each competition, write a function that returns the winner of the tournament. The Input arrays are named competitions and results, respectively. The competitions array has elements in the form of [homeTeam, awayTeam], where each team is a string of at most 30 characters representing the name of the team. The results array contains information about the winner of each corresponding competition in the competitions array. Specifically, results [1] denotes the winner of competitions[1], where a 1 in the results array means that the home team in the corresponding competition won and a means that the away team won. It's guaranteed that exactly one team will win the tournament and that each team will compete against all other teams exactly once. It's also guaranteed that the tournament will always have at least two teams.
+
+def tournamentWinner(competitions, results):
+    final = []
+    for i in range(len(results)):
+        final.append(competitions[i][abs((results[i]-1))])
+
+    return most_frequent(final)
+
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+      
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+  
+    return num
