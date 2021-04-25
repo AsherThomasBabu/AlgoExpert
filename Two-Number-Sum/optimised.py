2e1e422cf6f2d27c22554cfb59b434ec303db3eb
AsherThomasBabu <ashertb2000@gmail.com>
2021-04-25 16:37:50 +0530

    added twoNumberSum - Optimised

---
+++ Two-Number-Sum/optimised.py
@@ -0,0 +1,17 @@
+# Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order. If no two numbers sum up to the target sum, the function should return an empty array.
+
+# Note that the target sum has to be obtained by summing two different integers in the array. you can't add a single integer to itself in order to obtain the target sum.
+
+# You can assume that there will be at most one pair of numbers summing up to the target sum.
+
+def twoNumberSum(array, targetSum):
+    for i in range(len(array)):
+        j = targetSum - array[i]
+        if j in array and i!=array.index(j):
+            return[array[i],j]
+    return[]
+
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+targetSum = 10
+
+print(twoNumberSum(array,targetSum))
