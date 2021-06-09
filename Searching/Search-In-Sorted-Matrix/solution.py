d947350ca4056ae222bc72139469a34c33ff9be7
AsherThomasBabu <ashertb2000@gmail.com>
2021-06-09 21:59:06 +0530

    added searchInSortedMatrix - optimised

--- Searching/Search-In-Sorted-Matrix/solution.py
+++ Searching/Search-In-Sorted-Matrix/solution.py
@@ -3,4 +3,13 @@
 # otherwise [-1, -1]
 
 def searchInSortedMatrix(matrix, target):
-    return -1
+    row = 0
+    col = len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[row][col] < target:
+            row += 1
+        else:
+            return [row,col]
+    return [-1,-1]
