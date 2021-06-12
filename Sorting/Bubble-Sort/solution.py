58693aa2b02294409c95edd7f0ab09ffc858b171
AsherThomasBabu <ashertb2000@gmail.com>
2021-06-12 12:07:22 +0530

    added comments

--- Sorting/Bubble-Sort/solution.py
+++ Sorting/Bubble-Sort/solution.py
@@ -1,5 +1,7 @@
 # Write a finction that takes in an array of integers and returns a sorted version of that array. Use Bubble sort algorithm to sort the array.
 
+#  Best and worst case, | Runs in O(n^2)\
+    
 def bubbleSort(array):
     for i in range(len(array)-1-i):
         for j in range(i,len(array)):
